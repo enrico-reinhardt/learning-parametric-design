6d055cdfe3fee0f645ba73134c7b9c3ac469d198
enrico-reinhardt <102954013+enrico-reinhardt@users.noreply.github.com>
2022-04-06 12:11:54 +0200

    task01-creation

---
+++ code/task-01/sketch.js
@@ -0,0 +1,14 @@
+// PRELOAD ASSETS
+// LOAD ASSETS > WHEN LOADED EXECUTE SETUP
+function preload() {}
+
+// SETUP > ONCE
+function setup() {
+    createCanvas(400, 400)
+}
+
+// THEN > DRAW > LOOP
+function draw() {
+    background(255, 128, 0)
+    rect(200, 0, 10, 400)
+}
