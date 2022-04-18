18cbf88a8d63c944852ca48b381087140b771afb
enrico-reinhardt <102954013+enrico-reinhardt@users.noreply.github.com>
2022-04-18 23:48:36 +0200

    Task02

---
+++ code/task-02/sketch.js
@@ -0,0 +1,37 @@
+// VARIABLES
+const canvasWidth = 400,
+    canvasHeight = 400,
+    size = 2,
+    columns = canvasWidth / size,
+    rows = canvasHeight / size
+
+// PRELOAD
+function preload() {}
+
+// SETUP
+function setup() {
+    createCanvas(canvasWidth, canvasHeight)
+    angleMode(DEGREES)
+}
+// DRAW
+function draw() {
+    // noLoop()
+    noStroke()
+
+    for (let x = 0; x < columns; x++) {
+        for (let y = 0; y < rows; y++) {
+            let randomX = random(0, 80) / 1.5
+            let randomY = random(0, 150) * 2
+            if (randomY <= x / 10) {
+                fill('rgba(255, 255, 255, .4)')
+            }
+            if (randomX >= y * sin(32)) {
+                fill('rgba(0,0,0, 0.3)')
+            }
+            if (randomX >= x * 0.5) {
+                fill('rgba(10,10,10, 0.1)')
+            }
+            square(x * size, y * size, size)
+        }
+    }
+}
