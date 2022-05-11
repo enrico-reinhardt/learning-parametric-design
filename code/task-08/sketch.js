41b3343df41a77d551af828d4b90a4d62d3ab94d
enrico-reinhardt <102954013+enrico-reinhardt@users.noreply.github.com>
2022-05-11 09:15:20 +0200

    sketch08 final

--- code/task-08/sketch.js
+++ code/task-08/sketch.js
@@ -4,15 +4,16 @@ function preload() {}
 // VARIABLES
 const sketchWidth = window.innerWidth;
 const sketchHeight = window.innerHeight;
-const shapeCount = 400;
+const shapeCount = 175;
 const size = 120;
-const scaleTreshold = 10;
+const scaleTreshold = 8;
+const scrollSteps = 12000;
 let shapes = [];
 
 const scroller = document.createElement('div');
 scroller.classList.add('scroller');
 document.body.append(scroller);
-scroller.style.height = '10000px';
+scroller.style.height = scrollSteps + 'px';
 
 // SETUP
 function setup() {
@@ -24,10 +25,8 @@ function setup() {
     // Generate Initial Shapes
     for (let count = 0; count < shapeCount; count++) {
         shapes.push({
-            x: size / 2,
-            y: size / 2,
             scale: (scaleTreshold / shapeCount) * count,
-            rotation: count * 45,
+            rotation: count * 2,
         });
     }
 }
@@ -44,21 +43,32 @@ function draw() {
     // noLoop();
     background(255, 255, 255);
     noFill();
+    strokeWeight(0.4);
     stroke(0);
-    strokeWeight(0.5);
 
     let scrollY = Math.round(window.scrollY);
     let factor = scrollY / 10;
+
+    if (scrollY < 400) {
+        fill(0, 0, 0, 255 - scrollY);
+        push();
+        // translate(-50, -50);
+        textSize(18);
+        textAlign(CENTER, CENTER);
+        text('Scroll down', windowWidth / 2, windowHeight / 2);
+        pop();
+        noFill();
+    }
 
     translate(windowWidth / 2, windowHeight / 2);
     for (let i = 0; i < shapes.length; i++) {
         push();
 
-        rotate(shapes[i].rotation);
+        rotate(shapes[i].rotation + scrollY / 900);
         translate(mouseX / 10 - factor, mouseY / 10 - factor);
         // strokeWeight(i * 0.1);
 
-        shapes[i].rotation += 0.001;
+        shapes[i].rotation += 0.0005;
 
         if (shapes[i].scale < scaleTreshold || shapes[i].scale < 0) {
             shapes[i].scale += 0.05;
@@ -69,7 +79,7 @@ function draw() {
         shapes[i].scale = Math.round(shapes[i].scale);
         scale(shapes[i].scale);
 
-        square(shapes[i].x, shapes[i].y, size);
+        square(size / 2, size / 2, size);
         pop();
     }
 }
